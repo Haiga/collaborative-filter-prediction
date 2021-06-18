28144ab84d39a656589bb15653fd73e3bae3214f
Pedro <pedro_rga@hotmail.com>
2021-06-18 17:50:32 -0300

    creating iterative when the input is too large

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
 
 
 def print_hi(name):
@@ -12,5 +13,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print(np.array_split(np.arange(7), 3))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
